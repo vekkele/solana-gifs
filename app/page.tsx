50993ce18e7eb6df4c24d9fd72c956996a8110fc
Vlad Keliy <vekkele@gmail.com>
2022-11-23 20:31:56 +0500

    :heavy_plus_sign: Add tailwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,15 @@
-import styles from './page.module.css';
 import WalletButton from '../components/WalletButton';
 import WalletContextProvider from '../components/WalletContextProvider';
 
 export default function Home() {
   return (
     <WalletContextProvider>
-      <header className={styles.header}>
+      <header className="flex justify-end p-4">
         <WalletButton />
       </header>
-      <main className={styles.main}>
-        <h1>Gifs</h1>
+      <main className="flex flex-col justify-center items-center flex-auto min-h-screen py-16 px-0">
+        <h1 className="text-4xl font-bold">Gifs</h1>
       </main>
     </WalletContextProvider>
-
   )
 }
