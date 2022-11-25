28068a5d55db71837500726b7b602235a45f15cf
Vlad Keliy <vekkele@gmail.com>
2022-11-25 22:33:12 +0500

    :sparkles: Add sample counting smart contract

--- programs/solana-gifs/src/lib.rs
+++ programs/solana-gifs/src/lib.rs
@@ -7,9 +7,36 @@ pub mod solana_gifs {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.total_gifs = 0;
+
+        Ok(())
+    }
+
+    pub fn add_gif(ctx: Context<AddGif>) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.total_gifs += 1;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+}
--- tests/solana-gifs.ts
+++ tests/solana-gifs.ts
@@ -1,16 +1,49 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+import { assert } from "chai";
 import { SolanaGifs } from "../target/types/solana_gifs";
 
 describe("solana-gifs", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
-
+  const { SystemProgram } = anchor.web3;
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
   const program = anchor.workspace.SolanaGifs as Program<SolanaGifs>;
+  const baseAccount = anchor.web3.Keypair.generate();
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    console.log("🚀 Initializing...");
+    const tx = await program.methods.initialize()
+      .accounts({
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([baseAccount])
+      .rpc();
+
+    console.log("📝 Your transaction signature", tx);
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    const totalGifs = account.totalGifs;
+    console.log("👀 Total gifs:", totalGifs);
+
+    const expected = 0;
+    assert(account.totalGifs.eqn(expected), `totalGifs equals ${totalGifs}. Expected: ${expected}`);
   });
+
+  it("Adds a gif", async () => {
+    console.log("🚀 Adding a gif...");
+    const tx = await program.methods.addGif()
+      .accounts({ baseAccount: baseAccount.publicKey })
+      .rpc();
+
+    console.log("📝 Your transaction signature", tx);
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    const totalGifs = account.totalGifs;
+    console.log("👀 Total gifs:", totalGifs);
+
+    const expected = 1;
+    assert(account.totalGifs.eqn(expected), `totalGifs equals ${totalGifs}. Expected: ${expected}`);
+  })
 });
