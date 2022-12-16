becc9d601e234e3a8be8a809022e374e43ad35d2
Vlad Keliy <vekkele@gmail.com>
2022-12-16 14:34:21 +0530

    :rocket: Update program id and deploy

--- programs/solana-gifs/src/lib.rs
+++ programs/solana-gifs/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("HuLCc7aj2iw2Zfb2XbVsNQpiixa6MhydxkSdf5CsMTvY");
 
 #[program]
 pub mod solana_gifs {
